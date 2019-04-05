c181591f23d79b7e5150489dc00cceb8eced65a8
Incun <sddo08@naver,com>
2019-04-05 09:22:47 +0900

    First

---
+++ Bamsongi/Assets/BamsongiController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiController : MonoBehaviour
+{
+    
+    float delta = 0;
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision Other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    void Start ()
+    {
+        //Shoot(new Vector3(0, 200, 2000));
+    }
+	
+	void Update ()
+    {
+        this.delta += Time.deltaTime;
+        if (delta > 2)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
